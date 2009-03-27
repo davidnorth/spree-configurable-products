7b44638fff88d8a26343bb2f8119078741de9fcc
David North <davidnorth@gmail.com>
2009-03-27 17:11:12 +0000

    handle empty option when selecting option type, return empty array to clear option value select

--- app/controllers/admin/product_option_values_controller.rb
+++ app/controllers/admin/product_option_values_controller.rb
@@ -4,9 +4,14 @@ class Admin::ProductOptionValuesController < Admin::BaseController
   belongs_to :product
     
   def values_for_select
-    @option_type = OptionType.find(params[:option_type_id])
+    begin
+      @option_type = OptionType.find(params[:option_type_id])
+      @data = @option_type.option_values.map{|ov| {:id => ov.id, :label => ov.presentation}}
+    rescue
+      @data = []
+    end
     respond_to do |wants|
-      wants.js { render :text => @option_type.option_values.map{|ov| {:id => ov.id, :label => ov.presentation}}.to_json }
+      wants.js { render :text => @data.to_json }
     end
   end
   
